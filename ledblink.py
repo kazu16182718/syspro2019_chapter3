1bdfd9d7acda859516cc602bd8788b935266ee6a
kazu16182718 <s16019@tokyo.kosen-ac.jp>
2019-09-26 02:06:24 +0100

    Lチカ

---
+++ ledblink.py
@@ -0,0 +1,16 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+TIME = 0.5 
+ROOP = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+for i in range(ROOP):
+    GPIO.output(14, GPIO.HIGH) 
+    time.sleep(TIME)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(TIME)
+
+GPIO.cleanup()
